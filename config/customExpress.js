fe974676e710d61713683cf0388d1bf41ded1670
Adriel Modollo <adrielmodollo@outlook.com>
2020-12-17 01:22:05 -0300

    body-parser

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require('express') // chama nosso pacote express
 const consign = require('consign') //chama nosso consign, agrupa todas nossas rotas e coloca tudo isso dentro no app
+const bodyParser = require('body-parser') // Usamos body-parser para conseguir ler os dados que estamos recebendo no body.
 
     module.exports = () => {
         const app = express() // cria nosso app express
+
+        app.use(bodyParser.urlencoded({extended: true})) //utiliza minha biblioteca body-parser
+        app.use(bodyParser.json()) // para arquivos com extensão.json
 
         consign() //chama consign
             .include('controllers') //inclue todos controllers dentro do consign
--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,6 +1,10 @@
 module.exports = app => { 
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um get'))//Passa o caminho da rota que será executado
     // '/' <- caminho ou rota ||||||| req <- faz a requisão |||||| res <- responde a requisição
+    
+    app.post('/atendimentos', (req, res) => { 
+        console.log(req.body)
+        res.send("Você está na rota de atendimentos e está realizando um POST") //O post ao invés de receber dados, ou seja pedir dados. O post envia dados para nosso servidor
 
-    app.post('./atendimentos', (req, res) => res.send("Você está na rota de atendimentos e está realizando um POST")) //O post ao invés de receber dados, ou seja pedir dados. O post envia dados para nosso servidor
-} //função que exporta nosso app
+    })
+}//função que exporta nosso app
