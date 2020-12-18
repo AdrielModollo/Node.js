8d75d406c23654e222b78232ccaaa70a21f65283
Adriel Modollo <adrielmodollo@outlook.com>
2020-12-18 00:26:02 -0300

    Adicionado validações de entrada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,6 +5,31 @@ class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo cinco caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        } else {
+
         const atendimentoDatado = {...atendimento, dataCriacao, data}
         
         const sql = 'INSERT INTO Atendimentos SET ?'
@@ -16,6 +41,7 @@ class Atendimento {
                res.status(201).json(resultados)
             }
         })
+        }
     }
 }
 
