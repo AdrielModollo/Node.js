232043ddad8ee914d8b16387f8dde59827ffdf37
Adriel Modollo <adrielmodollo@outlook.com>
2020-12-17 23:07:09 -0300

    Salvando Atendimentos no banco de dados

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,9 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => { 
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um get'))//Passa o caminho da rota que será executado
     // '/' <- caminho ou rota ||||||| req <- faz a requisão |||||| res <- responde a requisição
     
     app.post('/atendimentos', (req, res) => { 
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
         res.send("Você está na rota de atendimentos e está realizando um POST") //O post ao invés de receber dados, ou seja pedir dados. O post envia dados para nosso servidor
 
     })
